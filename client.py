dcb43e0a91e61093bdce63839fdc68b9f586ea42
achohbee <kprice@gmail.com>
2024-08-26 12:20:36 -0500

    Add dynamic channel (#2)
    Add support for dynamic channels. This includes:
    * a `/dc <channel>` command to add a dynamic channel
    * a timer that cleans up unoccupied dynamic channels after ~10 minutes
    
    Additionally, some refactoring and changes to pass mypy.

--- client.py
+++ client.py
@@ -2,6 +2,8 @@
 
 import discord
 
+from dynamicchannel import DynamicChannel
+from gamerole import GameRole
 from tree import BrawlBotCommandTree
 from util import info
 
@@ -10,10 +12,19 @@ class BrawlBotClient(discord.Client):
 
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
+
         self.tree = BrawlBotCommandTree(self)
+        self.dc = DynamicChannel(self)
+        self.gr = GameRole()
+
+        self.tree.add_command(self.gr)
+        self.tree.add_command(self.dc.generate_command())
+
 
     async def on_ready(self):
         """Register comamnds"""
         info("Registering command tree")
         await self.tree.sync()
+        info("Initializing Dynamic Channel check loop")
+        self.dc.check_channels.start()
         info("Ready to handle requests")
